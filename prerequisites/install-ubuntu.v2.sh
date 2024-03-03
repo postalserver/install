913e0a120d3da64b2ef351ca2a54b326a60d688b
Adam Cooke <adam@krystal.io>
2024-03-03 10:58:59 +0000

    feat: add prereq install scripts

---
+++ prerequisites/install-ubuntu.v2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install -y ca-certificates curl git jq
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+# Install docker
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install helper
+git clone https://postalserver.io/start/install /opt/postal/install
+ln -s /opt/postal/install/bin/postal /usr/bin/postal
+
+# Run MariaDB
+docker run -d \
+   --name postal-mariadb \
+   -p 127.0.0.1:3306:3306 \
+   --restart always \
+   -e MARIADB_DATABASE=postal \
+   -e MARIADB_ROOT_PASSWORD=postal \
+   mariadb
+
+# Run RabbitMQ
+docker run -d \
+   --name postal-rabbitmq \
+   -p 127.0.0.1:5672:5672 \
+   --restart always \
+   -e RABBITMQ_DEFAULT_USER=postal \
+   -e RABBITMQ_DEFAULT_PASS=postal \
+   -e RABBITMQ_DEFAULT_VHOST=postal \
+   rabbitmq:3.8
---
+++ prerequisites/install-ubuntu.v3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install -y ca-certificates curl git jq
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+# Install docker
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install helper
+git clone https://postalserver.io/start/install /opt/postal/install -b v3
+ln -s /opt/postal/install/bin/postal /usr/bin/postal
+
+# Run MariaDB
+docker run -d \
+   --name postal-mariadb \
+   -p 127.0.0.1:3306:3306 \
+   --restart always \
+   -e MARIADB_DATABASE=postal \
+   -e MARIADB_ROOT_PASSWORD=postal \
+   mariadb
